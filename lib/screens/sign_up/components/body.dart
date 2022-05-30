b88fae2f236b806ae7f19efa22f8cada7641bbf5
Mhossain028 <meraj.hossain028@gmail.com>
2022-05-30 12:07:06 +0600

    sign up

--- lib/screens/sign_up/components/body.dart
+++ lib/screens/sign_up/components/body.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:sena_community_app/helpers/constants/constants.dart';
 
-class Body extends StatelessWidget {
-  // final Key _formKey = GlobalKey<FormState>();
+class Body extends StatefulWidget {
   const Body({Key? key}) : super(key: key);
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  final TextEditingController _date = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -82,62 +90,194 @@ class Body extends StatelessWidget {
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(20)),
                 child: SizedBox(
-                  height: size.height * 0.57,
+                  height: size.height * 0.6,
                   width: size.width * 0.9,
                   child: Padding(
                     padding: const EdgeInsets.all(20),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        const TextField(
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(
-                            hintText: "Email",
-                            hintStyle: TextStyle(fontSize: 16),
-                            prefixIcon: Icon(
-                              Icons.mail,
-                              color: Colors.green,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          const TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: "Name",
+                              prefixIcon: Icon(
+                                Icons.person,
+                                color: Colors.green,
+                              ),
                             ),
                           ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        const TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            hintText: "Password",
-                            prefixIcon: Icon(
-                              Icons.key,
-                              color: Colors.green,
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const TextField(
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              hintText: "Email",
+                              hintStyle: TextStyle(fontSize: 16),
+                              prefixIcon: Icon(
+                                Icons.mail,
+                                color: Colors.green,
+                              ),
                             ),
                           ),
-                        ),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        SizedBox(
-                          width: size.width * 0.4,
-                          child: RawMaterialButton(
-                            fillColor: Colors.white,
-                            padding: const EdgeInsets.symmetric(vertical: 10.0),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20.0),
-                            ),
-                            onPressed: () {},
-                            child: const Text(
-                              "Sign Up",
-                              style: TextStyle(fontWeight: FontWeight.bold),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              hintText: "Password",
+                              prefixIcon: Icon(
+                                Icons.key,
+                                color: Colors.green,
+                              ),
                             ),
                           ),
-                        ),
-                      ],
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              hintText: "Confirm Password",
+                              prefixIcon: Icon(
+                                Icons.key,
+                                color: Colors.green,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const TextField(
+                            decoration: InputDecoration(
+                              hintText: "BA Number",
+                              prefixIcon: Icon(
+                                Icons.numbers,
+                                color: Colors.green,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const TextField(
+                            decoration: InputDecoration(
+                              hintText: "GC Number",
+                              prefixIcon: Icon(
+                                Icons.numbers,
+                                color: Colors.green,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            controller: _date,
+                            decoration: const InputDecoration(
+                              hintText: "Bitrh Date",
+                              prefixIcon: Icon(
+                                Icons.calendar_today,
+                                color: Colors.green,
+                              ),
+                            ),
+                            onTap: () async {
+                              DateTime? pickdate = await showDatePicker(
+                                  context: context,
+                                  initialDate: DateTime.now(),
+                                  firstDate: DateTime(1900),
+                                  lastDate: DateTime.now());
+                              (context, child) => Theme(
+                                    data: Theme.of(context).copyWith(
+                                      colorScheme: const ColorScheme.light(
+                                        primary: Colors.green,
+                                        onPrimary: Colors.green,
+                                        surface: Colors.green,
+                                      ),
+                                    ),
+                                    child: child,
+                                  );
+
+                              if (pickdate != null) {
+                                _date.text =
+                                    DateFormat('dd-MM-yyyy').format(pickdate);
+                              }
+                            },
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const TextField(
+                            decoration: InputDecoration(
+                              hintText: "Spouse Name",
+                              prefixIcon: Icon(
+                                Icons.person,
+                                color: Colors.green,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            controller: _date,
+                            decoration: const InputDecoration(
+                              hintText: "Bitrh Date",
+                              prefixIcon: Icon(
+                                Icons.calendar_today,
+                                color: Colors.green,
+                              ),
+                            ),
+                            onTap: () async {
+                              DateTime? pickdate = await showDatePicker(
+                                  context: context,
+                                  initialDate: DateTime.now(),
+                                  firstDate: DateTime(1900),
+                                  lastDate: DateTime.now());
+                              (context, child) => Theme(
+                                    data: Theme.of(context).copyWith(
+                                      colorScheme: const ColorScheme.light(
+                                        primary: Colors.green,
+                                        onPrimary: Colors.green,
+                                        surface: Colors.green,
+                                      ),
+                                    ),
+                                    child: child,
+                                  );
+
+                              if (pickdate != null) {
+                                _date.text =
+                                    DateFormat('dd-MM-yyyy').format(pickdate);
+                              }
+                            },
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          SizedBox(
+                            width: size.width * 0.4,
+                            child: RawMaterialButton(
+                              fillColor: Colors.white,
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 10.0),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0),
+                              ),
+                              onPressed: () {},
+                              child: const Text(
+                                "Sign Up",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
